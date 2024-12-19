39a876cc45d0f92c95f13385e049495b1f029e38
ironkrush <krush2752@gmail.com>
2024-12-19 23:31:22 +0530

    json formatting updated

--- src/pages/Styles.jsx
+++ src/pages/Styles.jsx
@@ -7,32 +7,32 @@ const Styles = () => {
   const { subcategoryId } = useParams();
 
   const category = productsData.find((cat) =>
-    cat.subcategories.some((subcat) => subcat.id === parseInt(subcategoryId))
+    cat.subcategories?.some((subcat) => subcat.id === parseInt(subcategoryId))
   );
-  const subcategory = category.subcategories.find(
+  const subcategory = category?.subcategories?.find(
     (subcat) => subcat.id === parseInt(subcategoryId)
   );
+
+  if (!category || !subcategory) {
+    return <div>Subcategory not found. Please check the URL or data.</div>;
+  }
 
   return (
     <div className="styles-container">
       <h2>{subcategory.subcategory}</h2>
       <div className="style-prnt">
-
         {subcategory.styles.map((style) => (
           <div key={style.id} className="style-card">
             <img src={style.image} alt={style.style} className="style-image" />
             <div className="style-footer">
               <p>{style.style}</p>
-              {/* <p>{style.description}</p> */}
               <Link to={`/style/${style.id}`}>
                 <button className="card__btn">View Options</button>
               </Link>
             </div>
           </div>
-
         ))}
       </div>
-
     </div>
   );
 };
