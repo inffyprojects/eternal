dc74684cc92ce92c0d76e84ebf00ccc77f67e1ca
ironkrush <krush2752@gmail.com>
2025-01-31 11:24:08 +0530

    form added for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/stylesheets/contact.css';
+import Swal from 'sweetalert2'
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "995e2a26-7ada-46d3-abbf-bd893c7dbbb4");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            Swal.fire({
+                title: "Success!",
+                text: "Message delivered!",
+                icon: "success"
+              });
+        }
+      };
+
     return (
         <div className="contact-main">
-            <div className="contact-content">
-                <div className="text-contact">
-                    <h1>Let's Get In Touch!</h1>
-                    <h3>Let’s bridge the distance! We're excited to connect and explore opportunities across borders.</h3>
-                </div>
-                <img className="world-map" src="contacteternal.svg" alt="World Map" />
-            </div>
 
+            <section className='contact'>
+                <form onSubmit={onSubmit}>
+                    <div className="input-box">
+                        <label>Full Name</label>
+                        <input type="text" className='field' placeholder='Enter Your Name' name='name' required />
+                    </div>
+                    <div className="input-box">
+                        <label>Email Address</label>
+                        <input type="email" className='field' placeholder='Enter Your Email' name='email' required />
+                    </div>
+                    <div className="input-box">
+                        <label>Mobile Number</label>
+                        <input type='number' className='field' placeholder='Enter Your Mobile Number' name='number'required />
+                    </div>
+                    <div className="input-box">
+                        <label>Your Message</label>
+                        <textarea name='message' className='field mess' placeholder='Enter Your Name' required />
+                    </div>
+                    <button type='submit'>Send Message</button>
+                </form>
+            </section>
+            
             <div className="contact-page2">
                 <div className="email-contact">
                     <h1>Mail Us</h1>
@@ -33,6 +77,6 @@ const Contact = () => {
             </div>
         </div>
     );
-}
+};
 
 export default Contact;
